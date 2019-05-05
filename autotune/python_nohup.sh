331e5da2302c753407abc31b10e6c017728fe3b9
kanxuan <kanxuan1996@gmail.com>
2019-05-05 19:42:09 +0800

    add readme

--- autotune/python_nohup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-a=$1
-b="${a%.*}.log"
-nohup python -u $1 > ./tmp/$b 2>&1 &
-b="nohup_pid_${a%.*}.txt"
-echo $! > ./tmp/$b
--- autotune/sh_nohup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-a=$1
-b="${a%.*}.log"
-nohup $1 > ./tmp/$b 2>&1 &
-b="nohup_pid.txt"
-echo $! > ./tmp/$b
